662fdb90e4185dcc3f7931e2e2956d416af5d78d
VKiruthi <102936828+VKiruthi@users.noreply.github.com>
2023-04-18 20:18:45 +0530

    Update App.py

--- App.py
+++ App.py
@@ -1,2 +1,12 @@
 import streamlit as st
-st.write('Hi')
+
+st.header('TDS Assignment')
+st.subheader('Find the largest among the 3 given numbers')
+
+with st.form(key='form1'):
+    n1 = st.number_input("Enter the First Number :")
+    n2 =st.number_input("Enter the Second Number :")
+    n3 =st.number_input("Enter the Third Number :")
+    button = st.form_submit_button("Find the maximum")
+if button:
+    st.success(f"The maximum number is {max([n1,n2,n3])} ")
